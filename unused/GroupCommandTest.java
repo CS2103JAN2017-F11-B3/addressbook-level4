c15588f8dba58372fa76cf8a0401ed4a774181dd
Malik J <SmartyMJ@users.noreply.github.com>
2017-04-10 11:31:37 +0800

    Update GroupCommandTest.java

--- unused/GroupCommandTest.java
+++ unused/GroupCommandTest.java
@@ -8,23 +8,18 @@ public class GroupCommandTest extends AddressBookGuiTest {
 
     @Test
     public void Group() {
-        
+
         //group with only one correspond person
         assertGroupResult("group1", td.alice);
-        
         //group with multiple correspond persons
         assertGroupResult("street", new TestPerson[]{td.daniel, td.george, td.carl});
-        
         //group an empty group
         assertGroupResult("emptyGroup");
-        
         //group after delete one person
         commandBox.runCommand("find Daniel");
         commandBox.runCommand("delete 1");
         assertGroupResult("street" , new TestPerson[]{td.george, td.carl});
-
     }
-    
     private void assertGroupResult(String group, TestPerson... expectedHits) {
         commandBox.runCommand("group " + group);
         assertListSize(expectedHits.length);
