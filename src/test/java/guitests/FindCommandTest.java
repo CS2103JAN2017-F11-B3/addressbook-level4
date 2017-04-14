2ed443621066acf8e8dbd248b5dd3ae2700a0957
DESKTOP-R8MVRC3\ASUS <tony83126@gmail.com>
2017-04-14 20:42:25 +0800

    FindCommandTest

--- src/test/java/guitests/FindCommandTest.java
+++ src/test/java/guitests/FindCommandTest.java
@@ -26,7 +26,7 @@ public class FindCommandTest extends AddressBookGuiTest {
     }
 
     @Test
-    public void findInvalidCommand_fail() {
+    public void findInvalidCommandFail() {
         commandBox.runCommand("findgeorge");
         assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
     }
